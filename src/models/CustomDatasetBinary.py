992f272aaed43e2b50fd09f42b6a7c8229567d4b
daniel <scalena99@gmail.com>
2023-04-17 15:27:24 +0200

    fix: bugfix on dtype usage

--- src/models/CustomDatasetBinary.py
+++ src/models/CustomDatasetBinary.py
@@ -1,11 +1,12 @@
 
 # imports
 from torch.utils.data import Dataset
+import torch
 import numpy as np
 
 
 class CustomDatasetBinary(Dataset):
-    def __init__(self, df, tokenizer, label_col = ''):
+    def __init__(self, df, tokenizer, label_col = '', dtype = torch.float32):
         self.text = tokenizer(
             df['text'].to_list(), 
             padding = 'max_length', 
@@ -14,6 +15,7 @@ class CustomDatasetBinary(Dataset):
             return_tensors = "pt",
         )
         self.labels = df[label_col].to_list()
+        self.dtype = dtype
 
     def __len__(self,):
         return len(self.text['input_ids'])
@@ -23,5 +25,5 @@ class CustomDatasetBinary(Dataset):
             'input_ids': self.text['input_ids'][idx],
             # 'token_type_ids': self.text['token_type_ids'][idx],
             'attention_mask': self.text['attention_mask'][idx],
-        }, np.float32(self.labels[idx])
+        }, torch.tensor(self.labels[idx], dtype = self.dtype)
     
